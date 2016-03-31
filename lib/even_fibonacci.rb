e475d10f44a3423730882e21200c60894676d7f9
Cordavi <michaelamundsen@gmail.com>
2016-03-31 11:54:32 -0400

    Completed lesson

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,48 @@
 # Implement your procedural solution here!
+def fibonacci(maximum)
+
+  currentIndex = 1
+  numberArray = [1, 2]
+  loop do
+    newValue = (numberArray[currentIndex] + numberArray[currentIndex - 1])
+    break if newValue > maximum
+    numberArray << newValue
+    currentIndex += 1
+  end 
+  
+  return numberArray
+  
+end
+
+
+
+def even_fibonacci_sum(maximum)
+ 
+  sequence =  fibonacci(maximum)
+  numbers = Array.new
+  
+  sequence.each do |i|
+    if i % 2 == 0
+      numbers << i
+    end
+  end
+
+  numbers.inject(0, :+)
+
+end
+
+class EvenFibonacci
+
+  def initalize(maximum)
+    @maximum = maximum
+  end
+
+  def fibonacci
+    Object.send(:fibonacci, @maximum) 
+  end
+  
+  def even_fibonacci_sum
+    Object.send(:even_fibonacci_sum, @maximum) 
+  end
+
+end 
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci 
+
+   def initialize(maximum)
+     @maximum = maximum
+   end
+
+  def sum
+      currentIndex = 1
+      numberArray = [1, 2]
+      sumOfNumbers = Array.new
+
+      loop do
+         newValue = (numberArray[currentIndex] + numberArray[currentIndex - 1])
+         break if newValue > @maximum
+         numberArray << newValue
+         currentIndex += 1
+      end 
+
+     numberArray.each do |i|
+       if i % 2 == 0
+         sumOfNumbers << i
+       end
+      end
+
+   return sumOfNumbers.inject(0, :+)
+   end
+
+end
